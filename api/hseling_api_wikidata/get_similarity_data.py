d8cbb360e375b7e62c3d2696d81fffa4fc04f394
Marina Pozhiadeva <m.a.pozhidaeva@gmail.com>
2020-03-23 02:09:17 +0300

    db search changed for similarities

--- api/hseling_api_wikidata/get_similarity_data.py
+++ api/hseling_api_wikidata/get_similarity_data.py
@@ -10,31 +10,38 @@ def connect_to_db(path_to_db):
     return cursor
 
 def get_from_db(cursor, ngram, col):
-
     res = cursor.execute("""SELECT {} from FREQUENCIES where ngram='{}'""".format(col, ngram)).fetchall()
+    print(col)
     res = json.loads(res[0][0])
+    print('\n\nres:', res, '\n\n')
     if type(res) == dict:
         res = [list(i) for i in sorted(res.items(), key=lambda x:x[1], reverse=True)]
     return res
 
 
-def get_data(ngram, freq="relative_frequencies_1918_2009", sim="top10_cosine_rel_values"):
+def get_data(ngram, freq="rel", sim="cosine"):
+    sim = "top_{}_{}".format(sim, freq)
+    freq = "rows_" + freq
+    # print(sim)
+    #print(freq)
+     
     path_to_db = os.path.join(BASE_DIR, "ngrams.db")
+    cursor = connect_to_db(path_to_db)
     # if pct_change:
     #     freq, sim = 'change_rates', 'top10_cosine_change_rate'
     # else:
     #     freq, sim = 'relative_frequencies_1918_2009', 'top10_cosine_rel_values'
+    
+    sims = get_from_db(cursor=cursor, ngram=ngram.lower(), col=sim)
 
-    cursor = connect_to_db(path_to_db)
-    sims = get_from_db(cursor=cursor, ngram=ngram, col=sim)
-
+    # print('\n\n', sims, '\n\n')
     res = {'ngrams':[], 'frequencies':[], 'similarities':[]}
     for arr in sims:
-        print(arr)
-
-
+        
         word = arr[0]
-
+        print('\n\n', word, '\n\n')
+        word = cursor.execute("""SELECT {} from FREQUENCIES where idx='{}'""".format('ngram', word)).fetchall()[0][0]
+        
         similarity = arr[1]
         frequencies = get_from_db(cursor=cursor, ngram=word, col=freq)
 
